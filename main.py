01cbcf9f72f1978629b0b80314ee6c734f4cd66f
Jokodola Esther <estherjokodola21@gmail.com>
2026-03-10 11:40:26 +0100

    added routes and HTMLResponses

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from fastapi-blog!")
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+# @app.get("/")
+# def home():
+#     return {"message": "Hello World"}
+
+posts: list[dict] = [ 
+    {
+        "id": 1,
+        "author": "Corey Schafer",
+        "content": "FastAPI is awesome",
+        "title": "This framework is really easy to use and super fast",
+        "date_posted":"April 20, 2025",
+        
+    },
+    {
+        "id": 2,
+        "author": "Jane Doe",
+        "content": "I love FastAPI",
+        "title": "FastAPI is the best framework for building APIs",
+        "date_posted":"April 21, 2025",
+    }
+]
+
+@app.get("/posts", response_class = HTMLResponse, include_in_schema=False)
+@app.get("/", response_class = HTMLResponse, include_in_schema=False)
+def home():
+    return f"<h1>{posts[0]['title']}</h1>"
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
